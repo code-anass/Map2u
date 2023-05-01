eb49df3b9bef28e3606fc025f575213fc915c1e9
Anas Naeem <mughals.ahn@gmail.com>
2023-05-01 06:42:28 +0500

    updates till 30 april

--- src/screens/hubungiKami/HubungiKami.jsx
+++ src/screens/hubungiKami/HubungiKami.jsx
@@ -12,50 +12,57 @@ function HubungiKami() {
     AOS.refresh();
   }, []);
   return (
-    <div className="hubungiKami" data-aos="fade-down" data-aos-duration="2000">
-      <div className="left">
-        <img src={hubungi} alt="" />
-      </div>
-      <div className="right">
-        <h3>HUBUNGI KAMI</h3>
-        <div className="contact">
-          <h5>Jabatan Perancangan Bandar dan Desa Semenanjung Malaysia</h5>
-          <img src={contact} />
-          <h5>Pusat Operasi Malaysia Urban Observatory (MUO)</h5>
-          <div className="address1">
-            <p>Blok F5, Kompleks F, Presint 1,</p>
-            <p>Pusat Pentadbiran Kerajaan</p>
-            <p>Persekutuan, 62675 Putrajaya, Malaysia</p>
-            <div className="cell">
-              <div className="row">
-                <BsTelephoneOutbound className="icon" />
-                <p>+(60) 3-8091 0000</p>
+    <div className="hubung">
+      <h2>HUBUNGI KAMI</h2>
+
+      <div
+        className="hubungiKami"
+        data-aos="fade-down"
+        data-aos-duration="2000"
+      >
+        <div className="left">
+          <img src={hubungi} alt="" />
+        </div>
+        <div className="right">
+          <div className="contact">
+            <h5>Jabatan Perancangan Bandar dan Desa Semenanjung Malaysia</h5>
+            <img src={contact} />
+            <h5>Pusat Operasi Malaysia Urban Observatory (MUO)</h5>
+            <div className="address1">
+              <p>Blok F5, Kompleks F, Presint 1,</p>
+              <p>Pusat Pentadbiran Kerajaan</p>
+              <p>Persekutuan, 62675 Putrajaya, Malaysia</p>
+              <div className="cell">
+                <div className="row">
+                  <BsTelephoneOutbound className="icon" />
+                  <p>+(60) 3-8091 0000</p>
+                </div>
+                <div className="row">
+                  <MdOutlineForwardToInbox className="icon2" />
+                  <p>muo@planmalaysia.gov.my</p>
+                </div>
+                <hr />
               </div>
-              <div className="row">
-                <MdOutlineForwardToInbox className="icon2" />
-                <p>muo@planmalaysia.gov.my</p>
-              </div>
-              <hr />
             </div>
-          </div>
-          <h5>Pentadbir Sistem </h5>
-          <div className="address1">
-            <div className="cell">
-              <div className="row">
-                <HiUser className="icon2" />
-                <p>Pegawai MUO</p>
+            <h5>Pentadbir Sistem </h5>
+            <div className="address1">
+              <div className="cell">
+                <div className="row">
+                  <HiUser className="icon2" />
+                  <p>Pegawai MUO</p>
+                </div>
+                <div className="row">
+                  <BsTelephoneOutbound className="icon" />
+                  <p>+(60) 3-8091 0000</p>
+                </div>
+                <hr />
               </div>
-              <div className="row">
-                <BsTelephoneOutbound className="icon" />
-                <p>+(60) 3-8091 0000</p>
-              </div>
-              <hr />
             </div>
-          </div>
-          <h5>Waktu Operasi</h5>
-          <div className="address1">
-            <p>Isnin - Jumaat 9:30 pagi - 5.00 petang</p>
-            <p>Sabtu - Ahad & Cuti Awam - Tutup</p>
+            <h5>Waktu Operasi</h5>
+            <div className="address1">
+              <p>Isnin - Jumaat 9:30 pagi - 5.00 petang</p>
+              <p>Sabtu - Ahad & Cuti Awam - Tutup</p>
+            </div>
           </div>
         </div>
       </div>
--- src/screens/utama/Home.jsx
+++ src/screens/utama/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./home.scss";
+import home from "../../assets/utama/bg_main.png";
 import AOS from "aos";
 function Home() {
   useEffect(() => {
@@ -7,7 +8,13 @@ function Home() {
     AOS.refresh();
   }, []);
   return (
-    <div className="home" data-aos="fade-down" data-aos-duration="1000"></div>
+    <div className="home" data-aos="fade-down" data-aos-duration="1000">
+      <img src={home} />
+      <div className="headings-main">
+        <h1>PLATFORM BANDAR PINTAR NEGARA</h1>
+        <h3>"ADVANCED ANALYTICS FOR FUTURE CITIES"</h3>
+      </div>
+    </div>
   );
 }
 export default Home;
